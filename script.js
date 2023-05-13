7bf8dc30ab13e12232137f60f6d27489d31cc732
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2023-05-13 14:51:14 +0500

    Test calculation function

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 
+const btn = document.querySelectorAll(".btn:not(.special):not(.slim)");
+const keypad = document.querySelector(".keypad");
+const main = keypad.querySelector(".main");
 
+let result = document.querySelector(".result");
+let expression = document.querySelector(".expression");
 
+main.addEventListener("click", e => ButtonInput(e));
+main.addEventListener("touch", e => ButtonInput(e));
 
+let ButtonInput = (e) => {
+    if(e.target.value === '=') {
+        return equate();
+    } else {
+        expression.textContent += e.target.value;
+    };
+};
 
+let equate = () => {
+    if(expression.textContent.includes('=')) {
+        console.log('foo');
+    } else {
+        result.textContent = calculator(expression.textContent);
+        expression.textContent += '=';
+    };
+};
+
+let clearAll = () => {
+    expression.textContent = '';
+    result.textContent = '';
+};
 
 let calculator = (eqn) => {
     const operands = [...eqn.match(/(\d+\.\d*)|\d+/g)];
-    const operator = [...eqn.match(/\+|\-|\/|\*/g)]; 
+    const operator = [...eqn.match(/\+|\-|÷|×/g)]; 
     
     const a = +operands[0];
     const b = +operands[1];
@@ -18,10 +45,10 @@ let calculator = (eqn) => {
       case ('-'):
         result = a - b;
         break;
-      case ('/'):
+      case ('÷'):
         result = a / b;
         break;
-      case ('*'):
+      case ('×'):
         result = a * b;
         break;
     };
