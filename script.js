6602e270b57ced408319127c837cc6305c017a1f
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2023-05-17 14:04:30 +0500

    Temporarily disabled calc function, add del+clear

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 
-const btn = document.querySelectorAll(".btn:not(.special):not(.slim)");
+const btn = document.querySelectorAll(".btn");
 const keypad = document.querySelector(".keypad");
-const main = keypad.querySelector(".main");
 
 let result = document.querySelector(".result");
 let expression = document.querySelector(".expression");
+let memory = "";
 
-main.addEventListener("click", e => ButtonInput(e));
-main.addEventListener("touch", e => ButtonInput(e));
+keypad.addEventListener("click", e => getInput(e));
+keypad.addEventListener("touch", e => getInput(e));
 
-let ButtonInput = (e) => {
-    if(e.target.value === '=') {
-        return equate();
-    } else {
-        expression.textContent += e.target.value;
-    };
+const init = () => {
+  expression.textContent =
+   result.textContent =
+    memory = "";
+  console.log("initialized");
 };
 
-let equate = () => {
-    if(expression.textContent.includes('=')) {
-        console.log('foo');
-    } else {
-        result.textContent = calculator(expression.textContent);
-        expression.textContent += '=';
-    };
+// let pressed = e => console.log(e.target);
+
+const getInput = e => {
+  switch(e.target.value) {
+    case(void(0)): return;
+    case("CLR"): return init();
+    case("DEL"): return del();
+    case("="): return console.log("=")
+    default:
+      expression.textContent += e.target.value
+      break;
+  };  
 };
 
-let clearAll = () => {
-    expression.textContent = '';
-    result.textContent = '';
-};
+init();
 
-let calculator = (eqn) => {
+const del = () => {
+  expression.textContent = expression.textContent.substring(0, expression.textContent.length -1);
+}
+
+const calculator = (eqn) => {
     const operands = [...eqn.match(/(\d+\.\d*)|\d+/g)];
     const operator = [...eqn.match(/\+|\-|÷|×/g)]; 
     
