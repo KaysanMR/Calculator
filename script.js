7dfdc6e426d6e1756f6abfdd35baf419cfd8e1f1
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2023-05-17 18:07:58 +0500

    General cleanup on code

--- script.js
+++ script.js
@@ -6,56 +6,60 @@ let result = document.querySelector(".result");
 let expression = document.querySelector(".expression");
 let memory = "";
 
+let a = 0;
+let b = 0;
+let operator = '+'
+
 keypad.addEventListener("click", e => getInput(e));
 keypad.addEventListener("touch", e => getInput(e));
 
 const init = () => {
-  expression.textContent =
-   result.textContent =
-    memory = "";
+  expression.textContent = "";
+  result.textContent = "0";
+  memory = "";
   console.log("initialized");
 };
-
-// let pressed = e => console.log(e.target);
 
 const getInput = e => {
   switch(e.target.value) {
     case(void(0)): return;
     case("CLR"): return init();
-    case("DEL"): return del();
-    case("="): return console.log("=")
+    case("DEL"): return console.log('');
     default:
-      expression.textContent += e.target.value
+      operator? a += e.target.value : b += e.target.value; 
+      a += e.target.value;
+      if (/\+|\-|÷|×/g.test(e.target.value)) operator = e.target.value;
       break;
   };  
 };
 
-init();
+const splitInput = eqn => {
+  operands = [...eqn.match(/(\d+\.\d*)|\d+/g)],
+  operator = [...eqn.match(/\+|\-|÷|×/g)], 
+  a = +operands[0],
+  b = +operands[1];
+  return;
+};
 
-const del = () => {
-  expression.textContent = expression.textContent.substring(0, expression.textContent.length -1);
-}
+const calculate = () => {
 
-const calculator = (eqn) => {
-    const operands = [...eqn.match(/(\d+\.\d*)|\d+/g)];
-    const operator = [...eqn.match(/\+|\-|÷|×/g)]; 
-    
-    const a = +operands[0];
-    const b = +operands[1];
-    
-    switch (operator[0]) {
-      case ('+'):
-        result = a + b;
-        break;
-      case ('-'):
-        result = a - b;
-        break;
-      case ('÷'):
-        result = a / b;
-        break;
-      case ('×'):
-        result = a * b;
-        break;
+  switch (operator[0]) {
+    case ('+'):
+      result = a + b;
+      break;
+    case ('-'):
+      result = a - b;
+      break;
+    case ('÷'):
+      result = a / b;
+      break;
+    case ('×'):
+      result = a * b;
+      break;
     };
+
     return result % 1 != 0? result.toFixed(2):result; //only show decimals if the value is not rounded
-  };
+};
+          
+          
+init();
