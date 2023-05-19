bb39b919b67cd697770dc6698261ab35f5f65bcc
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2023-05-19 15:49:50 +0500

    Add new calculator functions

--- script.js
+++ script.js
@@ -4,19 +4,13 @@ const keypad = document.querySelector(".keypad");
 
 let result = document.querySelector(".result");
 let expression = document.querySelector(".expression");
-let memory = "";
-
-let a = 0;
-let b = 0;
-let operator = '+'
 
 keypad.addEventListener("click", e => getInput(e));
 keypad.addEventListener("touch", e => getInput(e));
 
 const init = () => {
   expression.textContent = "";
-  result.textContent = "0";
-  memory = "";
+  result.textContent = "";
   console.log("initialized");
 };
 
@@ -25,41 +19,86 @@ const getInput = e => {
     case(void(0)): return;
     case("CLR"): return init();
     case("DEL"): return console.log('');
+    case("="): return result.textContent = calculatinator(expression.textContent);
     default:
-      operator? a += e.target.value : b += e.target.value; 
-      a += e.target.value;
-      if (/\+|\-|÷|×/g.test(e.target.value)) operator = e.target.value;
+      expression.textContent += e.target.value;
       break;
   };  
 };
 
-const splitInput = eqn => {
-  operands = [...eqn.match(/(\d+\.\d*)|\d+/g)],
-  operator = [...eqn.match(/\+|\-|÷|×/g)], 
-  a = +operands[0],
-  b = +operands[1];
-  return;
+//super cool calculator code stuff
+
+const regEx = {
+  operands : /([-+]?)((\d+\.\d*)|\d+)/g,
+  divide : /([-+]?)((\d+\.\d*)|\d+)[÷\/]([-+]?)((\d+\.\d*)|\d+)/g,
+  multiply : /([-+]?)((\d+\.\d*)|\d+)[×\*]([-+]?)((\d+\.\d*)|\d+)/g,
 };
 
-const calculate = () => {
-
-  switch (operator[0]) {
-    case ('+'):
-      result = a + b;
-      break;
-    case ('-'):
-      result = a - b;
-      break;
-    case ('÷'):
-      result = a / b;
-      break;
-    case ('×'):
-      result = a * b;
-      break;
-    };
-
-    return result % 1 != 0? result.toFixed(2):result; //only show decimals if the value is not rounded
+const divideAll = eqn => {
+  
+  const divSplit = eqn => {
+    operands = [...eqn.match(regEx.divide)]
+    return operands;
+  };
+  
+  const divide = eqn => {
+    operands = [...eqn.match(regEx.operands)];
+    let quotient = parseFloat(operands[0])/parseFloat(operands[1]);
+    return quotient;
+  };
+  
+  while (eqn.includes("÷") || eqn.includes("/")) {
+    
+    divSplit(eqn);
+    operands.forEach(item => {
+      eqn = eqn.replace(item, `+${divide(item)}`);
+    });
+    
+  };
+  
+  return eqn;  
+  
 };
-          
-          
+
+const multiplyAll = eqn => {
+  
+  const mulSplit = eqn => {
+    operands = [...eqn.match(regEx.multiply)]
+    return operands;
+  };
+  
+  const multiply = eqn => {
+    operands = [...eqn.match(regEx.operands)];
+    let product = parseFloat(operands[0])*parseFloat(operands[1]);
+    return product;
+  };
+  
+  while (eqn.includes("×") || eqn.includes("*")) {
+    
+    mulSplit(eqn);
+    operands.forEach(item => {
+      eqn = eqn.replace(item, `+${multiply(item)}`);
+    });
+    
+  };
+  
+  return eqn;  
+  
+};
+
+const sumAll = eqn => {
+  
+  const operands = [...eqn.match(regEx.operands)];
+
+  let sum = 0;
+  operands.forEach(item => sum += parseFloat(item));
+  return sum;
+  
+};
+
+const calculatinator = eqn => {
+  out = sumAll(multiplyAll(divideAll(eqn)));
+  return out % 1 != 0? out.toFixed(2):out; //only show decimals if the value is not rounded
+};
+ 
 init();
